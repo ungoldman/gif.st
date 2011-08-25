95b31276036d6b5183e0dd1e2a39b6c2b65acb7d
Nate Goldman <nnmgoldman@gmail.com>
2011-08-25 01:08:14 -0700

    changed /request to /login

--- controller.rb
+++ controller.rb
@@ -5,25 +5,25 @@ class Controller < Sinatra::Base
     
     consumer_key = ENV['consumer_key']
     consumer_secret = ENV['consumer_secret']
-
+    
     @consumer ||= OAuth::Consumer.new(consumer_key, consumer_secret, :site => "http://twitter.com")
-
+    
     if !session[:oauth][:request_token].nil? && !session[:oauth][:request_token_secret].nil?
       @request_token = OAuth::RequestToken.new(@consumer, session[:oauth][:request_token], session[:oauth][:request_token_secret])
     end
-
+    
     if !session[:oauth][:access_token].nil? && !session[:oauth][:access_token_secret].nil?
       @access_token = OAuth::AccessToken.new(@consumer, session[:oauth][:access_token], session[:oauth][:access_token_secret])
     end
-
+    
     if @access_token
       @client = Grackle::Client.new(:auth => {
         :type => :oauth,
         :consumer_key => consumer_key,
         :consumer_secret => consumer_secret,
-        :token => @access_token.token, 
+        :token => @access_token.token,
         :token_secret => @access_token.secret
-      })    
+      })
     end
   end
   
@@ -32,24 +32,24 @@ class Controller < Sinatra::Base
       @statuses = @client.statuses.friends_timeline? :count => 100
       erubis :index
     else
-      '<a href="/request">Sign On</a>'
+      '<a href="/login">Sign On</a>'
     end
   end
   
-  get '/request/?' do
+  get '/login/?' do
     @request_token = @consumer.get_request_token(:oauth_callback => 'http://gif.herokuapp.com/auth')
     session[:oauth][:request_token] = @request_token.token
     session[:oauth][:request_token_secret] = @request_token.secret
     redirect @request_token.authorize_url
   end
-
+  
   get '/auth/?' do
     @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
     session[:oauth][:access_token] = @access_token.token
     session[:oauth][:access_token_secret] = @access_token.secret
     redirect '/'
   end
-
+  
   get '/logout/?' do
     session[:oauth] = {}
     redirect '/'
